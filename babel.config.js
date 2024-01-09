ae6e9b1248710eb0a7f85730beb0f2da07d8f7e6
Will Douglas <willygdouglas@gmail.com>
2024-01-09 13:38:24 -0700

    Prettier format all

--- babel.config.js
+++ babel.config.js
@@ -2,8 +2,8 @@
 /* eslint-env node */
 
 module.exports = {
-    presets: [
-      ['@babel/preset-env', {targets: {node: 'current'}}],
-      '@babel/preset-typescript',
-    ],
-  };
+  presets: [
+    ["@babel/preset-env", { targets: { node: "current" } }],
+    "@babel/preset-typescript",
+  ],
+};
