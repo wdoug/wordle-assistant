4e10c9e0a9ff5210b518a3ffc029caa4d633e68f
Will Douglas <willygdouglas@gmail.com>
2024-01-10 22:49:20 -0700

    Add initial implementation for filtering down remaining words
    This is still missing some logic and should definitely be refactored

--- index.test.ts
+++ index.test.ts
@@ -1,5 +1,68 @@
-import { hello } from "./index";
+import { analyzeBoard, Board, BoardWord } from "./index";
 
-test("hello is World", () => {
-  expect(hello).toBe("World");
+describe("analyzeBoard", () => {
+  it("works with everything empty", () => {
+    const result = analyzeBoard([], [], []);
+    expect(result.possibleRemainingWords).toEqual([]);
+  });
+
+  it("works with words but no board information", () => {
+    const possibleFinalWords = ["abcde", "fghij"];
+    const result = analyzeBoard([], [], possibleFinalWords);
+    expect(result.possibleRemainingWords).toEqual(possibleFinalWords);
+  });
+
+  it("properly removes words with unused letters", () => {
+    const possibleFinalWords = ["abcde", "fghij"];
+    const unusedLetters = [..."aaaaa"];
+    const board = [
+      unusedLetters.map((l) => ({ letter: l, state: "unused" })),
+    ] as Board;
+    const result = analyzeBoard(board, [], possibleFinalWords);
+    expect(result.possibleRemainingWords).toEqual(["fghij"]);
+  });
+
+  it("properly removes words with letters in the wrong place", () => {
+    const possibleFinalWords = ["abcde"];
+    const boardWord: BoardWord = [
+      { letter: "a", state: "differentSpot" },
+      { letter: "z", state: "unused" },
+      { letter: "z", state: "unused" },
+      { letter: "z", state: "unused" },
+      { letter: "z", state: "unused" },
+    ];
+    const board = [boardWord];
+    const result = analyzeBoard(board, [], possibleFinalWords);
+    expect(result.possibleRemainingWords).toEqual([]);
+  });
+
+  it("properly removes words missing letters in the right place", () => {
+    const possibleFinalWords = ["abcde"];
+    const boardWord: BoardWord = [
+      { letter: "b", state: "correctSpot" },
+      { letter: "z", state: "unused" },
+      { letter: "z", state: "unused" },
+      { letter: "z", state: "unused" },
+      { letter: "z", state: "unused" },
+    ];
+    const board = [boardWord];
+    const result = analyzeBoard(board, [], possibleFinalWords);
+    expect(result.possibleRemainingWords).toEqual([]);
+  });
+
+  it("properly removes words that are missing a required letter", () => {
+    const possibleFinalWords = ["abcde"];
+    const boardWord: BoardWord = [
+      { letter: "f", state: "differentSpot" },
+      { letter: "z", state: "unused" },
+      { letter: "z", state: "unused" },
+      { letter: "z", state: "unused" },
+      { letter: "z", state: "unused" },
+    ];
+    const board = [boardWord];
+    const result = analyzeBoard(board, [], possibleFinalWords);
+    expect(result.possibleRemainingWords).toEqual([]);
+  });
+
+  it.todo("properly removes words that have too many of a given letter");
 });
--- index.ts
+++ index.ts
@@ -1,3 +1,101 @@
-export const hello = "World";
+type Output = {
+  possibleWordCount: number;
+  possibleRemainingWords: string[];
+  suggestedNextWords: string[];
+};
 
-console.log("Hello, world");
+export type Character = {
+  letter: string;
+  state: "unused" | "correctSpot" | "differentSpot";
+};
+
+export type BoardWord = [Character, Character, Character, Character, Character];
+
+export type Board = BoardWord[];
+
+export function analyzeBoard(
+  board: Board,
+  allowedInputWords: string[],
+  possibleFinalWords: string[],
+) {
+  const wordLength = 5;
+  const correctLetters = Array.from(Array(wordLength));
+  type NotAllowedLetterLocations = { [character: string]: Set<number> };
+  const notAllowedLetterLocations: NotAllowedLetterLocations = {};
+  const unusedLetters = new Set();
+  type LetterCount = { [character: string]: number };
+  const knownLetterCounts: LetterCount = {};
+  // TODO: Figure out multiple character
+  for (const boardWord of board) {
+    const knownWordLetterCounts: LetterCount = {};
+    for (let i = 0; i < wordLength; i++) {
+      const char = boardWord[i];
+      if (!char) {
+        throw new Error(
+          `Unexpected missing board character. Tried to index a character at ${i} in a boardWord of length ${boardWord.length}`,
+        );
+      }
+
+      if (char.state === "unused") {
+        unusedLetters.add(char.letter);
+      } else if (char.state === "correctSpot") {
+        correctLetters[i] = char.letter;
+        knownWordLetterCounts[char.letter] =
+          (knownWordLetterCounts[char.letter] ?? 0) + 1;
+      } else if (char.state === "differentSpot") {
+        knownWordLetterCounts[char.letter] =
+          (knownWordLetterCounts[char.letter] ?? 0) + 1;
+
+        if (!notAllowedLetterLocations[char.letter]) {
+          notAllowedLetterLocations[char.letter] = new Set();
+        }
+        notAllowedLetterLocations[char.letter].add(i);
+      }
+    }
+    for (const letter in Object.keys(knownWordLetterCounts)) {
+      knownLetterCounts[letter] = Math.max(
+        knownWordLetterCounts[letter] ?? 0,
+        knownLetterCounts[letter] ?? 0,
+      );
+    }
+  }
+
+  const possibleRemainingWords = possibleFinalWords.filter((possibleWord) => {
+    const possibleWordLetterCounts: LetterCount = {};
+    for (let i = 0; i < wordLength; i++) {
+      const letter = possibleWord[i];
+      if (!letter) {
+        throw new Error(
+          `Unexpected possible word ${possibleWord} that is shorter than the expected word length: ${wordLength}`,
+        );
+      }
+      possibleWordLetterCounts[letter] =
+        (possibleWordLetterCounts[letter] ?? 0) + 1;
+
+      const correctLetter = correctLetters[i];
+      if (
+        (correctLetter && correctLetter !== letter) ||
+        unusedLetters.has(letter) ||
+        notAllowedLetterLocations[letter]?.has(i)
+      ) {
+        return false;
+      }
+    }
+
+    for (const letter in Object.keys(knownLetterCounts)) {
+      const possibleWordLetterCount = possibleWordLetterCounts[letter];
+      if (
+        !possibleWordLetterCount ||
+        possibleWordLetterCount < knownLetterCounts[letter]
+      ) {
+        return false;
+      }
+    }
+
+    return true;
+  });
+
+  return {
+    possibleRemainingWords,
+  };
+}
